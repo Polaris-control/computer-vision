9c1376bbdd47bf8d457c9f9ed77d21798fa139c8
HU kaifeng <74716458+Polaris-control@users.noreply.github.com>
2024-04-03 14:59:07 +0900

    Update Canny Edge Detection.py

--- Canny Edge Detection.py
+++ Canny Edge Detection.py
@@ -2,135 +2,140 @@ from PIL import Image
 import math
 import numpy as np
 
-"""
-Get and use the functions associated with gaussconvolve2d that you used in the last HW02.
-"""
 def gauss1d(sigma):
     size = int(math.ceil(6 * sigma))
-    x = np.arange(-size, size + 1)
+    x = np.arange(-size//2, size//2 + 1)
     kernel = np.exp(-x**2 / (2 * sigma**2))
     kernel /= np.sum(kernel)
     return kernel
 
 def gauss2d(sigma):
     size = int(math.ceil(6 * sigma))
-    x, y = np.meshgrid(np.arange(-size, size + 1), np.arange(-size, size + 1))
+    x, y = np.meshgrid(np.arange(-size//2, size//2 + 1), np.arange(-size//2, size//2 + 1))
     kernel = np.exp(-(x**2 + y**2) / (2 * sigma**2))
     kernel /= np.sum(kernel)
     return kernel
 
-def gaussconvolve2d(array, sigma):
-    # Generate 2D Gaussian filter
-    kernel = gauss2d(sigma)
-    
-    # Perform 2D convolution
-    kernel_size = len(kernel)
+def convolve2d(array, filter):
+    kernel_size = len(filter)
     image_height, image_width = array.shape
     padded_array = np.pad(array, ((kernel_size//2, kernel_size//2), (kernel_size//2, kernel_size//2)), mode='edge')
     result = np.zeros_like(array)
 
     for i in range(image_height):
         for j in range(image_width):
-            result[i, j] = np.sum(padded_array[i:i+kernel_size, j:j+kernel_size] * kernel)
+            result[i, j] = np.sum(padded_array[i:i+kernel_size, j:j+kernel_size] * filter)
 
     return result
 
+def gaussconvolve2d(array, sigma):
+    kernel = gauss2d(sigma)
+    return convolve2d(array, kernel)
+
 def reduce_noise(img):
-    """ Return the gray scale gaussian filtered image with sigma=1.6
-    Args:
-        img: RGB image. Numpy array of shape (H, W, 3).
-    Returns:
-        res: gray scale gaussian filtered image (H, W).
-    """
-    #implement
-    return res
+    grayscale_img = img.convert("L")
+    grayscale_array = np.array(grayscale_img, dtype=np.float32)
+    filtered_array = gaussconvolve2d(grayscale_array, 1.6)
+    return filtered_array
 
 def sobel_filters(img):
-    """ Returns gradient magnitude and direction of input img.
-    Args:
-        img: Grayscale image. Numpy array of shape (H, W).
-    Returns:
-        G: Magnitude of gradient at each pixel in img.
-            Numpy array of shape (H, W).
-        theta: Direction of gradient at each pixel in img.
-            Numpy array of shape (H, W).
-    Hints:
-        - Use np.hypot and np.arctan2 to calculate square root and arctan
-    """
-    #implement 
-    return (G, theta)
+    sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    sobel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
+    Ix = convolve2d(img, sobel_x)
+    Iy = convolve2d(img, sobel_y)
+
+    G = np.hypot(Ix, Iy)
+    theta = np.arctan2(Iy, Ix)
+
+    G = (G / np.max(G)) * 255
+
+    return G, theta
 
 def non_max_suppression(G, theta):
-    """ Performs non-maximum suppression.
-    This function performs non-maximum suppression along the direction
-    of gradient (theta) on the gradient magnitude image (G).
-    Args:
-        G: gradient magnitude image with shape of (H, W).
-        theta: direction of gradients with shape of (H, W).
-    Returns:
-        res: non-maxima suppressed image.
-    """
-    pass
+    H, W = G.shape
+    res = np.zeros((H, W))
+
+    theta *= 180 / np.pi
+    theta[theta < 0] += 180
+
+    for i in range(1, H - 1):
+        for j in range(1, W - 1):
+            direction = theta[i, j]
+
+            if (0 <= direction < 22.5) or (157.5 <= direction <= 180) or (67.5 <= direction < 112.5):
+                q = G[i, j+1]
+                r = G[i, j-1]
+            elif (22.5 <= direction < 67.5) or (112.5 <= direction < 157.5):
+                q = G[i+1, j-1]
+                r = G[i-1, j+1]
+            elif (67.5 <= direction < 112.5) or (157.5 <= direction < 202.5):
+                q = G[i+1, j]
+                r = G[i-1, j]
+            elif (112.5 <= direction < 157.5) or (22.5 <= direction < 67.5):
+                q = G[i+1, j+1]
+                r = G[i-1, j-1]
+
+            if (G[i, j] >= q) and (G[i, j] >= r):
+                res[i, j] = G[i, j]
+
     return res
+
+def dfs(weak_edges, strong_edges, i, j, visited):
+    stack = [(i, j)]
+    visited.add((i, j))
+
+    while stack:
+        curr_i, curr_j = stack.pop()
+        strong_edges[curr_i, curr_j] = 255
+
+        for ii in range(curr_i-1, curr_i+2):
+            for jj in range(curr_j-1, curr_j+2):
+                if weak_edges[ii, jj] == 80 and (ii, jj) not in visited:
+                    stack.append((ii, jj))
+                    visited.add((ii, jj))
 
 def double_thresholding(img):
-    """ 
-    Args:
-        img: numpy array of shape (H, W) representing NMS edge response.
-    Returns:
-        res: double_thresholded image.
-    """
-    #implement     
-    return res
+    diff = np.max(img) - np.min(img)
+    high_threshold = np.min(img) + diff * 0.15
+    low_threshold = np.min(img) + diff * 0.03
 
-def dfs(img, res, i, j, visited=[]):
-    # calling dfs on (i, j) coordinate imply that
-    #   1. the (i, j) is strong edge
-    #   2. the (i, j) is weak edge connected to a strong edge
-    # In case 2, it meets the condition to be a strong edge
-    # therefore, change the value of the (i, j) which is weak edge to 255 which is strong edge
-    res[i, j] = 255
+    strong_edges = np.where(img >= high_threshold, 255, 0)
+    weak_edges = np.where((img >= low_threshold) & (img < high_threshold), 80, 0)
 
-    # mark the visitation
-    visited.append((i, j))
+    H, W = weak_edges.shape
+    visited = set()
 
-    # examine (i, j)'s 8 neighbors
-    # call dfs recursively if there is a weak edge
-    for ii in range(i-1, i+2) :
-        for jj in range(j-1, j+2) :
-            if (img[ii, jj] == 80) and ((ii, jj) not in visited) :
-                dfs(img, res, ii, jj, visited)
+    for i in range(1, H - 1):
+        for j in range(1, W - 1):
+            if weak_edges[i, j] == 80 and (i, j) not in visited:
+                dfs(weak_edges, strong_edges, i, j, visited)
 
-def hysteresis(img):
-    """ Find weak edges connected to strong edges and link them.
-    Iterate over each pixel in strong_edges and perform depth first
-    search across the connected pixels in weak_edges to link them.
-    Here we consider a pixel (a, b) is connected to a pixel (c, d)
-    if (a, b) is one of the eight neighboring pixels of (c, d).
-    Args:
-        img: numpy array of shape (H, W) representing NMS edge response.
-    Returns:
-        res: hysteresised image.
-    """
-    #implement 
+    return strong_edges
 
-    return res
+def hysteresis(array):
+    strong_edges = np.where(array == 255)
+    weak_edges = np.where(array == 80)
+
+    for i, j in zip(strong_edges[0], strong_edges[1]):
+        dfs(array, array, i, j)
+
+    return array
 
 def main():
     RGB_img = Image.open('./iguana.bmp')
 
     noise_reduced_img = reduce_noise(RGB_img)
-    Image.fromarray(noise_reduced_img.astype('uint8')).save('./iguana_blurred.bmp', 'BMP')
+    Image.fromarray(noise_reduced_img.astype('uint8')).show()
     
     g, theta = sobel_filters(noise_reduced_img)
-    Image.fromarray(g.astype('uint8')).save('./iguana_sobel_gradient.bmp', 'BMP')
-    Image.fromarray(theta.astype('uint8')).save('./iguana_sobel_theta.bmp', 'BMP')
+    Image.fromarray(g.astype('uint8')).show()
+    Image.fromarray(theta.astype('uint8')).show()
 
     non_max_suppression_img = non_max_suppression(g, theta)
-    Image.fromarray(non_max_suppression_img.astype('uint8')).save('./iguana_non_max_suppression.bmp', 'BMP')
+    Image.fromarray(non_max_suppression_img.astype('uint8')).show()
 
     double_threshold_img = double_thresholding(non_max_suppression_img)
-    Image.fromarray(double_threshold_img.astype('uint8')).save('./iguana_double_thresholding.bmp', 'BMP')
+    Image.fromarray(double_threshold_img.astype('uint8')).show()
 
-    hysteresis_img = hysteresis(double_threshold_img)
-    Image.fromarray(hysteresis_img.astype('uint8')).save('./iguana_hysteresis.bmp', 'BMP')
+main()
